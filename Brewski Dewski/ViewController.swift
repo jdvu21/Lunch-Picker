95f57709a840cb58b020ee9994464cbb11ff8873
Mike Weyandt <mike@mikeweyandt.com>
2015-12-01 23:22:00 -0500

    Working on linking restaurants to users via reference list on user.

--- Brewski Dewski/ViewController.swift
+++ Brewski Dewski/ViewController.swift
@@ -9,63 +9,114 @@
 import UIKit
 import CloudKit
 
+
+//Sorting public should not be difficult, linking resturants to users will be difficult
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        
-        
-        
-        //creates naming issues, better to let CloudKit handle naming on its own
-        //let resturantRecordID = CKRecordID(recordName: "NewResturant")
-        
-        
-        let resturant = CKRecord(recordType: "Resturant")
-        
-        
-        resturant["LastVisit"] = NSDate()
-        resturant["Location"] = CLLocation(latitude: 15, longitude: 105)
-        resturant["Name"] = "Wendys"
-        resturant["Rating"] = 5
-        resturant["Type"] = "Fast Food"
-        
-        
         let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
-        //        //publicDatabase.saveRecord(resturant, completionHandler: <#T##(CKRecord?, NSError?) -> Void#>)
-        //        publicDatabase.saveRecord(resturant) {
-        //            resturant, error in
-        //            if error != nil {
-        //                print(error!.localizedDescription)
-        //
-        //            } else {
-        //                print("Should have been saved")
-        //            }
-        //        }
-        
-        let predicate = NSPredicate(format: "Rating == 4")
-        
-        let query = CKQuery(recordType: "Resturant", predicate: predicate)
-        
-        
-        publicDatabase.performQuery(query, inZoneWithID: nil) {
-            results, error in
+        //Will probably inclose all CloudKit stuff, cannot do much without UserID
+        CKContainer.defaultContainer().fetchUserRecordIDWithCompletionHandler() {
+            myID, error in
             
             if error != nil {
                 print(error!.localizedDescription)
-            } else {
-                let result0 = results![0]
-                print(result0["Name"]!)
+                return
             }
+        
+            guard let myID = myID else {
+                print("Unable to get unique user ID")
+                return
+            }
+            
+            publicDatabase.fetchRecordWithID(myID) {
+                ID, error in
+                
+                guard let ID = ID else {
+                    print("Unable to find ID")
+                    return
+                }
+                
+                
+                guard var myResturants = ID["MyResturants"] as? [CKReference] else {
+                    print("You do not have any resturants!")
+                    return
+                }
+                
+//                for resturant in myResturants {
+//                    print(resturant.recordID.recordName)
+//                }
+                
+                //print(myResturants[2])
+                
+                let newRef = CKReference(recordID: myResturants[0].recordID, action: CKReferenceAction.None)
+                //function that takes resturant, returns CKReference to new object in CloudKit to add to array of references....
+                
+                
+                
+                myResturants.append(newRef)
+                
+                print(myResturants)
+                //TODO: Figure out how to save a reference to the list of resturants under users...
+                
+                
         }
-        
-        
-        
-        
-        
+            
+            
+
+
+
+//        //creates naming issues, better to let CloudKit handle naming on its own
+//        //let resturantRecordID = CKRecordID(recordName: "NewResturant")
+//        
+//        
+//        let resturant = CKRecord(recordType: "Resturant")
+//        
+//        
+//        resturant["LastVisit"] = NSDate()
+//        resturant["Location"] = CLLocation(latitude: 15, longitude: 105)
+//        resturant["Name"] = "Taco Bell"
+//        resturant["Rating"] = 3
+//        resturant["Type"] = "Fast Food"
+//        
+//        
+//        //let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
+//        //        //publicDatabase.saveRecord(resturant, completionHandler: <#T##(CKRecord?, NSError?) -> Void#>)
+//                publicDatabase.saveRecord(resturant) {
+//                    resturant, error in
+//                    if error != nil {
+//                        print(error!.localizedDescription)
+//        
+//                    } else {
+//                        print("Should have been saved")
+//                    }
+//                }
+//        
+            
+            
+            // Searching for suggestions....
+//        let ratingPredicate = NSPredicate(format: "Rating == 4")
+//        
+//        let query = CKQuery(recordType: "Resturant", predicate: ratingPredicate)
+//        
+//        
+//        publicDatabase.performQuery(query, inZoneWithID: nil) {
+//            results, error in
+//            
+//            if error != nil {
+//                print(error!.localizedDescription)
+//            } else {
+//                let result0 = results![0]
+//                print(result0["Name"]!)
+//            }
+//        }
+//        
         
     }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
