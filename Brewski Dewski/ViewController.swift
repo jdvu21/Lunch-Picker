3916381e97b4acc964f7fd8b67c9d072453f4bf6
Mike Weyandt <mike@mikeweyandt.com>
2015-12-14 13:24:56 -0500

    A little work on views

--- Brewski Dewski/ViewController.swift
+++ Brewski Dewski/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 import CloudKit
 
+struct Restuant {
+    
+    var name: String
+    var type: String
+    var favoriteDish: String?
+    
+    
+}
+
 
 //Sorting public should not be difficult, linking resturants to users will be difficult
 class ViewController: UIViewController {
@@ -17,10 +26,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var newPlaceType: UITextField!
     @IBOutlet weak var newPlaceFavoriteDish: UITextField?
     
+    //  Submit new place
+    @IBOutlet weak var newPlaceSubmit: UIButton!
+    
+    
+    //  When "Submit" is pressed, newplace is saved to public CloudKit and to personal plist data.
+    
+
+    //
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
     }
     
 
